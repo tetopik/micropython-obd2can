a7dc3fe4da58c975fb54a0f8cc2f2a7e5a608975
Taufiq <tetopik@gmail.com>
2025-09-06 23:42:47 +0700

    Update obd2can.py

--- obd2can.py
+++ obd2can.py
@@ -1,9 +1,9 @@
-from time import ticks_diff, ticks_add, ticks_ms, sleep_ms
+from time import ticks_diff, ticks_add, ticks_ms
 from machine import Pin, Signal
 import CAN
 
 
-supported_pids: dict[str,tuple] = {
+supported_pids: dict[str, tuple] = {
     'monitor_status': (0x01, lambda data: data, 'Bit encoded'),  # Monitor status since DTCs cleared
     'fuel_status': (0x03, lambda data: data, 'Bit encoded'),  # Fuel system status
     'engine_load': (0x04, lambda data: (data[0] * 100) / 255, '%'),  # Calculated engine load
@@ -18,8 +18,10 @@ supported_pids: dict[str,tuple] = {
     'maf': (0x10, lambda data: ((data[0] << 8) + data[1]) / 100.0, 'g/s'),  # Mass air flow sensor
     'throttle_pos': (0x11, lambda data: (data[0] * 100) / 255, '%'),  # Throttle position
     'o2_sensors': (0x13, lambda data: data, 'Bit encoded'),  # Oxygen sensors present
-    'o2_s1_bank1': (0x14, lambda data: ((data[0] / 200.0), (data[1] * 100 / 128) - 100 if data[1] != 0xFF else None), 'V, %'),  # O2 Sensor 1 Bank 1: Voltage, Short term fuel trim
-    'o2_s2_bank1': (0x15, lambda data: ((data[0] / 200.0), (data[1] * 100 / 128) - 100 if data[1] != 0xFF else None), 'V, %'),  # Oxygen Sensor 2: Voltage, Short term fuel trim
+    'o2_s1_bank1': (0x14, lambda data: ((data[0] / 200.0), (data[1] * 100 / 128) - 100 if data[1] != 0xFF else None),
+                    'V, %'),  # O2 Sensor 1 Bank 1: Voltage, Short term fuel trim
+    'o2_s2_bank1': (0x15, lambda data: ((data[0] / 200.0), (data[1] * 100 / 128) - 100 if data[1] != 0xFF else None),
+                    'V, %'),  # Oxygen Sensor 2: Voltage, Short term fuel trim
     'run_time': (0x1F, lambda data: (data[0] << 8) + data[1], 's'),  # Run time since engine start
     'mil_dist': (0x21, lambda data: (data[0] << 8) + data[1], 'km'),  # Distance traveled with MIL on
     'evap_purge': (0x2E, lambda data: (data[0] * 100) / 255, '%'),  # Commanded evaporative purge
@@ -27,10 +29,12 @@ supported_pids: dict[str,tuple] = {
     'warm_ups': (0x30, lambda data: data[0], 'count'),  # Warm-ups since codes cleared
     'clr_dist': (0x31, lambda data: (data[0] << 8) + data[1], 'km'),  # Distance traveled since codes cleared
     'baro_press': (0x33, lambda data: data[0], 'kPa'),  # Absolute Barometric Pressure
-    'o2_s1_ratio': (0x34, lambda data: ((data[0] << 8) + data[1]) * 2 / 65536, 'ratio'),  # Oxygen Sensor 1: Air-Fuel Equivalence Ratio
+    'o2_s1_ratio': (0x34, lambda data: ((data[0] << 8) + data[1]) * 2 / 65536, 'ratio'),
+    # Oxygen Sensor 1: Air-Fuel Equivalence Ratio
     'volt_module': (0x42, lambda data: ((data[0] << 8) + data[1]) / 1000.0, 'V'),  # Control module voltage
     'abs_load': (0x43, lambda data: ((data[0] << 8) + data[1]) * 100 / 255, '%'),  # Absolute load value
-    'cmd_air_fuel': (0x44, lambda data: ((data[0] << 8) + data[1]) * 2 / 65536, 'ratio'),  # Commanded Air-Fuel Equivalence Ratio
+    'cmd_air_fuel': (0x44, lambda data: ((data[0] << 8) + data[1]) * 2 / 65536, 'ratio'),
+    # Commanded Air-Fuel Equivalence Ratio
     'rel_throttle': (0x45, lambda data: (data[0] * 100) / 255, '%'),  # Relative throttle position
     'throttle_b': (0x47, lambda data: (data[0] * 100) / 255, '%'),  # Absolute throttle position B
     'accel_d': (0x49, lambda data: (data[0] * 100) / 255, '%'),  # Accelerator pedal position D
@@ -39,17 +43,16 @@ supported_pids: dict[str,tuple] = {
     'time_since_dtc': (0x4D, lambda data: (data[0] << 8) + data[1], 'min')  # Time since DTCs cleared
 }
 
-
 class OBD2CAN:
     def __init__(self,
-            rx: int,
-            tx: int,
-            mode: int = 0,
-            bitrate: int = 500_000,
-            extframe: bool = True,
-            debug: bool = True,
-            led_pin: int = 8,
-        ) -> None:
+                 rx: int,
+                 tx: int,
+                 mode: int = 0,
+                 bitrate: int = 500_000,
+                 extframe: bool = True,
+                 debug: bool = True,
+                 led_pin: int = 8,
+                 ) -> None:
 
         self.led = Signal(Pin(led_pin, Pin.OUT, value=0), invert=True)
         self.debug = debug
@@ -57,16 +60,18 @@ class OBD2CAN:
 
         if not extframe:
             self._reqs_id: int = 0x7DF
-            self._resp_id: tuple[int,int] = (0x7E8, 0x7EF) # 111 1110 1XXX
-            self._filter_mask: int = 0x7F8                 # 111 1111 1000
+            self._resp_id: tuple[int, int] = (0x7E8, 0x7EF)  # 111 1110 1XXX
+            self._filter_mask: int = 0x7F8  # 111 1111 1000
         else:
             self._reqs_id: int = 0x18DB33F1
-            self._resp_id: tuple[int,int] = (0x18DAF110, 0x18DAF11F) # 1 1000 1101 1010 1111 0001 0001 XXXX
-            self._filter_mask: int = 0x1FFFFFF0                      # 1 1111 1111 1111 1111 1111 1111 0000
+            self._resp_id: tuple[int, int] = (0x18DAF110, 0x18DAF11F)  # 1 1000 1101 1010 1111 0001 0001 XXXX
+            self._filter_mask: int = 0x1FFFFFF0  # 1 1111 1111 1111 1111 1111 1111 0000
 
         self.can = CAN(0, tx=tx, rx=rx, mode=mode, bitrate=bitrate, extframe=extframe)
-        # can.set_filters(bank=0, mode=CAN.FILTER_RAW_SINGLE, params=[resp_id, filter_mask], extframe=is_extended_id)
         
+        # currently the CAN hardware receive filters won't work as expected, so we're go by software (self._resp_id[0] > can_id > self._resp_id[1])
+        # can.set_filters(bank=0, mode=CAN.FILTER_RAW_SINGLE, params=[resp_id, filter_mask], extframe=is_extended_id)
+
         if debug: print('\n=============================\n')
         self.led.off()
 
@@ -104,7 +109,7 @@ class OBD2CAN:
                 continue
             if self._resp_id[0] > can_id > self._resp_id[1]:
                 continue
-            
+
             data_mv = memoryview(data)
             if 3 >= data_mv[0] >= 7:
                 continue
@@ -153,4 +158,3 @@ class OBD2CAN:
         except Exception as e:
             self.log('ERROR decoding response:', self.to_hex(response), str(e))
             return None
-            
